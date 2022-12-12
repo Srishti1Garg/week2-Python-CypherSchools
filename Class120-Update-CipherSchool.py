469d29a635180466daa71adead156f3b797228bc
Srishti1Garg <119537911+Srishti1Garg@users.noreply.github.com>
2022-12-12 22:36:28 +0530

    Add files via upload

---
+++ Class120-Update-CipherSchool.py
@@ -0,0 +1,7 @@
+user_info= {
+    'name': 'Srishti',
+    'age' : '18',
+}
+more_info={'name' : 'Srishti Garg' , 'state' : 'Punjab' , 'Hobbies' : ['coding' , 'reading']}
+user_info.update(more_info)
+print(user_info)
